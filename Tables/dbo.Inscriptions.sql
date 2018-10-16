165a45ea7544cd8f02e49bf55e7e417fc9840868
Ivo Miller <ivo.miller@red-gate.com>
2018-10-16 15:10:24 +0100

    Clobber

--- Tables/dbo.Inscriptions.sql
+++ Tables/dbo.Inscriptions.sql
@@ -1,7 +1,7 @@
 CREATE TABLE [dbo].[Inscriptions]
 (
 [Inscription Id] [nchar] (10) COLLATE Latin1_General_CI_AS NOT NULL,
-[CityState] [nchar] (50) COLLATE Latin1_General_CI_AS NULL
+[City Location] [nchar] (50) COLLATE Latin1_General_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Inscriptions] ADD CONSTRAINT [PK_Inscriptions] PRIMARY KEY CLUSTERED  ([Inscription Id]) ON [PRIMARY]
